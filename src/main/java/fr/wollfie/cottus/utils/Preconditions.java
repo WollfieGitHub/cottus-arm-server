c5550a47ca578a6e607ef569800be4950e8fad61
wollfie <leowolllff@gmail.com>
2022-12-09 08:03:03 +0100

    Add description for scholar papers

---
+++ src/main/java/fr/wollfie/cottus/utils/Preconditions.java
@@ -0,0 +1,21 @@
+﻿package fr.wollfie.cottus.utils;
+
+public class Preconditions {
+    private Preconditions() {}
+
+    /**
+     * Check that the given argument is true and throws an {@link  IllegalArgumentException} otherwise
+     * @param shouldBeTrue An argument that should be true
+     */
+    public static void checkArgument(boolean shouldBeTrue) {
+        if (!shouldBeTrue) { throw new IllegalArgumentException(); }
+    }
+
+    /**
+     * Check that the given argument is not null and throws an {@link  IllegalArgumentException} otherwise
+     * @param notNullObj An argument that should be not null
+     */
+    public static void checkNotNull(Object notNullObj) {
+        if (notNullObj == null) { throw new IllegalArgumentException(); }
+    }
+}
