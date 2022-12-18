3779178e3925705fe690b267dfdbab35a4dcb7c3
wollfie <leowolllff@gmail.com>
2022-12-18 09:58:25 +0100

    Complete implementation of BezierToAnimation

--- src/main/java/fr/wollfie/cottus/models/animation/pathing/BezierToAnimation.java
+++ src/main/java/fr/wollfie/cottus/models/animation/pathing/BezierToAnimation.java
@@ -2,37 +2,52 @@
 
 import fr.wollfie.cottus.utils.maths.Vector3D;
 import fr.wollfie.cottus.utils.maths.rotation.Rotation;
-import io.smallrye.mutiny.tuples.Tuple;
 import io.smallrye.mutiny.tuples.Tuple3;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
-public class BezierToAnimation extends EndEffectorAnimation{
-    
-    private final Vector3D endPosition;
+import static fr.wollfie.cottus.utils.Utils.binom;
+import static java.lang.Math.pow;
+
+public class BezierToAnimation extends EndEffectorAnimation {
+
     private final double timeSec;
-    private final Vector3D[] anchorPoints;
+    private final List<Vector3D> anchorPoints;
 
     public BezierToAnimation(boolean relative, Vector3D endPosition, double timeSec, Vector3D... anchorPoints) {
         super(relative);
-        this.endPosition = endPosition;
         this.timeSec = timeSec;
-        this.anchorPoints = anchorPoints;
+        this.anchorPoints = new ArrayList<>();
+
+        // First and last coefficient of the curve is end and begin points
+        this.anchorPoints.add(Vector3D.Zero);
+        this.anchorPoints.addAll(List.of(anchorPoints));
+        this.anchorPoints.add(endPosition);
     }
 
     @Override
-    public double getDurationSecs() { return timeSec; }
+    public double getDurationSecs() {
+        return timeSec;
+    }
 
+    /**
+     * @param secFromStart Seconds from start of the animation
+     * @return The specification for the end effector
+     * @implNote <a href="https://mathcurve.com/courbes3d.gb/bezier3d/bezier3d.shtml">Source</a>
+     */
     @Override
     protected Tuple3<Vector3D, Rotation, Double> relativeEvaluateAt(double secFromStart) {
-        double[] bezierCoefficient = new double[anchorPoints.length];
-        // TODO BEZIER COEFFICIENTS
-        // https://mathcurve.com/courbes3d.gb/bezier3d/bezier3d.shtml
-        
+
+        double t = secFromStart / timeSec;
+        int n = anchorPoints.size();
         Vector3D result = Vector3D.Zero;
-        for (int i = 0; i < anchorPoints.length; i++) {
-            result = result.add( anchorPoints[i].scaledBy(bezierCoefficient[i]) );
+
+        for (int k = 0; k < n; k++) {
+            double bernsteinCoefficient = binom(k, n) * pow(t, k) * pow(1 - t, n - k);
+            result = result.add(anchorPoints.get(k).scaledBy(bernsteinCoefficient));
         }
-        return Tuple3.of( result, Rotation.Identity, 0.0 );
+
+        return Tuple3.of(result, Rotation.Identity, 0.0);
     }
 }
--- src/main/java/fr/wollfie/cottus/utils/Utils.java
+++ src/main/java/fr/wollfie/cottus/utils/Utils.java
@@ -1,11 +1,24 @@
 package fr.wollfie.cottus.utils;
 
+import fr.wollfie.cottus.utils.Constants;
+
 import static fr.wollfie.cottus.utils.Constants.EPSILON;
 
 public class Utils {
     private Utils() {}
     
+    /** @return True if the double {@code v} is smaller than {@link Constants#EPSILON} */
     public static boolean isZero(double v) {
         return Math.abs(v) < EPSILON;
+    }
+    
+    /** @return The binomial coefficient for parameters (k, n)
+     * @implNote <a href="https://rosettacode.org/wiki/Evaluate_binomial_coefficients#Java">Source</a> */
+    public static double binom(int k, int n) {
+        if (k>n-k) { k=n-k; }
+
+        long b=1;
+        for (int i=1, m=n; i<=k; i++, m--) { b=b*m/i; }
+        return b;
     }
 }
